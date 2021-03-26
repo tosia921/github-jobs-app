bdde8c06324610da4eba0b27527894a5cb074959
Kamila Tarnożek <tomaszposiadala@gmail.com>
2021-03-26 13:14:50 +0000

    added styled components and normalize.css librarys, created GlobalStyles and Typography files, and added base styles set from given design files

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+      //Primary Colors
+    --Violet: #9e7f66;
+    --LightViolet: #939BF4;
+    --VeryDarkBlue: #19202D;
+    --Midnight: #121721 ;
+      //Secondary Colors
+    --White: #FFFFFF;
+    --LightGrey: #F4F6F8;
+    --Gray: #9DAEC2;
+    --DarkGrey: #6E8098 ;
+  }
+
+  html {
+    background-color: var(--white);
+    font-size: 10px;
+  }
+
+  body {
+    font-size: 2rem;
+    box-sizing: border-box;
+  }
+
+  img {
+    max-width: 100%;
+  }
+`;
+
+export default GlobalStyles;
---
+++ src/styles/Typography.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Typography = createGlobalStyle`
+  html {
+    font-family: 'Kumbh Sans', sans-serif; 
+    color: var(--VeryDarkBlue);
+  }
+  p, li {
+    font-size: 1.6rem;
+    font-weight: 400;
+    margin: 0;
+  }
+  h1 {
+    font-size: 2.8rem;
+    font-weight: 700;
+    margin: 0; 
+  }
+  h2 {
+    font-size: 2.4rem;
+    font-weight: 700;
+    margin: 0;
+  }
+  h3 {
+    font-size: 2rem;
+    font-weight: 700;
+    margin: 0;
+  }
+  h4 {
+    font-size: 1.4rem;
+    font-weight: 700;
+    margin: 0;
+  }
+  a {
+    color: var(--VeryDarkBlue);
+    text-decoration-color: none;
+  }
+ 
+  .center {
+    text-align: center;
+  }
+
+`;
+
+export default Typography;
