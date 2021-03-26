a55c6f686e66922433725e51bff4a663b55b627b
Kamila Tarnożek <tomaszposiadala@gmail.com>
2021-03-26 18:25:07 +0000

    added all the website asset's from exported design file to the assets folder, created custom hook to check for screen size, will use that in future header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import useWindowSize from './custom-hooks/useWindowSize';
+
 function App() {
+    const [height, width] = useWindowSize();
+
     return (
         <div className="App">
             <h1>tomek</h1>
+            height: {height}, width: {width}
         </div>
     );
 }
